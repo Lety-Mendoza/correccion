ec8a447100f16312ffb8006d7536160cf16fb792
ejemploCRUD <blackmoontese.2627@gmail.com>
2022-02-10 18:22:30 -0600

    correccion

---
+++ CRUD-main/ejemploCRUD/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
+
+//COMPONENTES A UTILIZAR
+import { AppComponent } from './app.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateUserComponent,
+    ListUserComponent,
+    NavbarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
